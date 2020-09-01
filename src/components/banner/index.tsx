310f06d031c7ebc614a9368d0a17042be6b0f2db
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-09-01 17:55:01 -0300

    Show Date Component 🔥
    Feat $banner changed

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button } from '@material-ui/core'
 import './index.css'
+import SearchDate from '../searchDate'
 
 function Banner() {
 
@@ -12,6 +13,7 @@ function Banner() {
                 <Button onClick={() => setShowSearch(!showSearch)} variant='outlined' className='banner__searchButton'>
                     Search Dates
                 </Button>
+                {showSearch && <SearchDate />}
             </div>
             <div className='banner__info'>
                 <h1>
